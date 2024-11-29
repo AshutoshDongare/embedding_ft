b3635bad8a49368cebbcbe9a8ba69cb9dad79564
Ashutosh Dongare <ashutosh.dongare@gmail.com>
2024-11-29 17:23:32 +0530

    Update finetune_embedding.py

--- finetune_embedding.py
+++ finetune_embedding.py
@@ -35,5 +35,38 @@ embed_model = finetune_engine.get_finetuned_model()
 # from tqdm.notebook import tqdm
 # import pandas as pd
 
+# def evaluate(
+#     dataset,
+#     embed_model,
+#     top_k=5,
+#     verbose=False,
+# ):
+#     corpus = dataset.corpus
+#     queries = dataset.queries
+#     relevant_docs = dataset.relevant_docs
+
+#     nodes = [TextNode(id_=id_, text=text) for id_, text in corpus.items()]
+#     index = VectorStoreIndex(
+#         nodes, embed_model=embed_model, show_progress=True
+#     )
+#     retriever = index.as_retriever(similarity_top_k=top_k)
+
+#     eval_results = []
+#     for query_id, query in tqdm(queries.items()):
+#         retrieved_nodes = retriever.retrieve(query)
+#         retrieved_ids = [node.node.node_id for node in retrieved_nodes]
+#         expected_id = relevant_docs[query_id][0]
+#         is_hit = expected_id in retrieved_ids  # assume 1 relevant doc
+
+#         eval_result = {
+#             "is_hit": is_hit,
+#             "retrieved": retrieved_ids,
+#             "expected": expected_id,
+#             "query": query_id,
+#         }
+#         eval_results.append(eval_result)
+#     return eval_results
+
+
 
 
